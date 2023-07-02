2a1215b9a3ea663e184864c357090c3a8b063bd7
munenepeter <peternjega7@gmail.com>
2023-07-02 17:22:29 +0300

    testing the new interface

---
+++ Views/test.view.php
@@ -0,0 +1,11 @@
+<?php include_once 'base.view.php'; ?>
+
+<div class="antialiased bg-gray-50 dark:bg-gray-900">
+    <!-- Nav -->
+    <?php include_once 'sections/nav.view.php'; ?>
+    <!-- Nav -->
+
+    <!-- Sidebar -->
+    <?php include_once 'sections/sidebar.view.php'; ?>
+
+</div>
