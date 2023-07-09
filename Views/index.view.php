338690f02ccc0693d31a949927c7d4de1d4cc202
munenepeter <peternjega7@gmail.com>
2023-07-09 16:35:50 +0300

    view day's event is good to go

--- Views/index.view.php
+++ Views/index.view.php
@@ -7,18 +7,16 @@
 
     <!-- Sidebar -->
     <?php include_once 'sections/sidebar.view.php'; ?>
-    <!-- Side bar -->
 
-
-    <main class="p-4 md:ml-32 h-auto pt-20 flex justify-between">
+    <main class="bg-white pt-4 ml-32 mt-2">
         <style>
             [x-cloak] {
                 display: none;
             }
         </style>
 
-        <section x-data="calendar()" class="w-full rounded-md border">
-            <div class="flex justify-between bg-gray-100 p-4 rounded-md">
+        <nav x-data="calendar()" class="w-full rounded-md border-b mt-8">
+            <div class="flex justify-between bg-gray-50 p-2">
                 <p><span class="font-semibold" x-text="`${getMonthName(month)}, ${year}`">January 22,
                         2022</span><br><span class="text-sm text-gray-400">Saturday</span></p>
                 <div class="flex items-center justify-end space-x-4">
@@ -40,9 +38,9 @@
 
 
                     <!-- Modal toggle -->
-                    <div class="flex justify-center m-5">
+                    <div class="flex justify-center p-2">
                         <button id="defaultModalButton" data-modal-toggle="defaultModal"
-                            class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                             type="button">
                             Book
                         </button>
@@ -77,14 +75,16 @@
                                     <div class="grid gap-4 mb-4 sm:grid-cols-2">
                                         <div>
                                             <label for="name"
-                                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Event Name</label>
+                                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Event
+                                                Name</label>
                                             <input type="text" name="name" id="name"
                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                 placeholder="Type an event name" required="">
                                         </div>
                                         <div>
                                             <label for="event_type"
-                                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Event Type</label>
+                                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Event
+                                                Type</label>
                                             <select id="event_type"
                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                                 <option selected="">Select Event Type</option>
@@ -96,7 +96,8 @@
                                         </div>
                                         <div>
                                             <label for="start_time"
-                                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start Time</label>
+                                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start
+                                                Time</label>
                                             <input type="time" name="start_time" id="start_time"
                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                 placeholder="08:00" required="">
@@ -104,12 +105,14 @@
                                         <div>
                                             <label for="duration"
                                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Duration</label>
-                                            <input type="text" name="duration" id="duration" required pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{2}" title="Write a duration in the format hh:mm:ss:ms"
+                                            <input type="text" name="duration" id="duration" required
+                                                pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{2}"
+                                                title="Write a duration in the format hh:mm:ss:ms"
                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                 placeholder="hh:mm:ss:ms">
                                         </div>
-                                      
-                                       
+
+
                                         <div class="sm:col-span-2">
                                             <label for="description"
                                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
@@ -136,88 +139,35 @@
                 </div>
 
             </div>
-            <!-- End of NAV -->
-            <article class="pt-4 bg-white flex">
-                <div class="overflow-y-auto max-h-96  w-3/4">
-                    <div class="ml-1 flex flex-col">
-                        <div class="flex-1">
-                            <div
-                                x-data="{ time: ['12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM'] }">
-                                <template x-for="(hour, index) in time" :key="index">
-                                    <div class="flex">
-                                        <div class="border-r border-gray-50 h-1/24 pb-2 w-1/12 -mr-4 -mt-1"
-                                            x-text="hour"></div>
-                                        <div class="bg-white border border-gray-100 w-10/12">
-                                            <div class="hover:bg-gray-100 border-b border-gray-100 h-10"></div>
-                                            <div class="hover:bg-gray-100 border-b border-gray-100 h-10"></div>
-                                        </div>
+
+
+        </nav>
+
+        <article class="w-full flex justify-around">
+        <section class="w-3/4 dark:bg-gray-700 bg-white rounded-b px-2">
+            <div class="overflow-y-auto max-h-96  w-full">
+                <div class="ml-1 flex flex-col">
+                    <div class="flex-1">
+                        <div
+                            x-data="{ time: ['12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM'] }">
+                            <template x-for="(hour, index) in time" :key="index">
+                                <div class="flex">
+                                    <div class="border-r border-gray-50 hover:border-t h-1/24 pb-2 w-1/12 -mr-4 -mt-1 text-sm text-gray-500" x-text="hour">
                                     </div>
-                                </template>
-                            </div>
+                                    <div class="bg-white border border-gray-100 w-10/12">
+                                        <div class="hover:bg-gray-100 border-b border-gray-100 h-10"></div>
+                                        <div class="hover:bg-gray-100 border-b border-gray-100 h-10"></div>
+                                    </div>
+                                </div>
+                            </template>
                         </div>
                     </div>
                 </div>
-                <!-- End of daily -->
-                <section class="-ml-12 w-1/4 bg-white p-1">
-                    <div>
-                        <div class="mb-4 text-center flex justify-between">
-                            <button class="mr-2" x-on:click="previousMonth"><svg xmlns="http://www.w3.org/2000/svg"
-                                    fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                    class="w-4 h-4">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M15.75 19.5L8.25 12l7.5-7.5" />
-                                </svg>
-                            </button>
-                            <h2 class="text-md font-semibold" x-text="`${getMonthName(month)} ${year}`"></h2>
-                            <button class="ml-2" x-on:click="nextMonth"><svg xmlns="http://www.w3.org/2000/svg"
-                                    fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                    class="w-4 h-4">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                                </svg>
-                            </button>
-                        </div>
-
-                        <section class="calendar">
-                            <div class="grid grid-cols-7 text-center text-gray-400">
-                                <p>S</p>
-                                <p>M</p>
-                                <p>T</p>
-                                <p>W</p>
-                                <p>T</p>
-                                <p>F</p>
-                                <p>S</p>
-                            </div>
-                            <div class="mt-1 rounded-md border bg-gray-50 text-center">
-                                <template x-for="week in weeks">
-                                    <div class="flex flex flex-wrap justify-evenly items-center text-center ">
-                                        <template x-for="day in week">
-                                            <form action="/events" method="get"
-                                                :class="{'bg-blue-500 text-white text-center w-[14.2857%] px-3 py-2': isToday(day) == true, 'w-[14.2857%] px-3 py-2 hover:bg-blue-200 text-center border-r border-b': isToday(day) == false }">
-                                                <input type="hidden" name="intent" value="checkEvent">
-                                                <input type="hidden" name="date"
-                                                    :value="`${day} ${getMonthName(month)} ${year}`">
-                                                <button role="button" type="submit" class="text-center" x-text="day">
-                                                </button>
-                                            </form>
-                                        </template>
-                                    </div>
-                                </template>
-                            </div>
-                        </section>
-                    </div>
-
-                </section>
-            </article>
-
-
-
+            </div>
         </section>
-
-    </main>
+        <?php include_once 'sections/calendar.view.php'; ?>
+        </article>
+       
 </div>
 
-
-
-
-
+</main>
