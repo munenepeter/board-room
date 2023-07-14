c5c4c60bd7eda1e013b5011300143366fbcdda3e
munenepeter <peternjega7@gmail.com>
2023-07-14 15:03:47 +0300

    update db dump

--- Core/Database/dump.sql
+++ Core/Database/dump.sql
@@ -62,7 +62,7 @@ CREATE TABLE `employees` (
 
 LOCK TABLES `employees` WRITE;
 /*!40000 ALTER TABLE `employees` DISABLE KEYS */;
-INSERT INTO `employees` VALUES (201041307,'mwaruguru','mwaruguru@kenyalaw.org','$2y$10$pJwuUF0LHnOvZ6ygCoI4iOmSBqq/MDCFAJvPJdvKfHINvrR06ZOrC');
+INSERT INTO `employees` VALUES (201041307,'mwaruguru','mwaruguru@kenyalaw.org','$2y$10$pJwuUF0LHnOvZ6ygCoI4iOmSBqq/MDCFAJvPJdvKfHINvrR06ZOrC'),(1001,'john_doe','john.doe@example.com','$2y$10$EwtP.50YnqK0LC/yKRO5du/vunq/d8fGcADaOw2MGUabBgDRsF7JK'),(1002,'jane_smith','jane.smith@example.com','$2y$10$8A1fQNYlSBQi55a5.g6T5.J8lJW6bfsvWs6nsr5w.V1KYN6nnUHzG'),(1003,'michael_johnson','michael.johnson@example.com','$2y$10$FZPK/4lswVRZmu0ZowkpQOnRpF8W6UOzILc/yzEZG/Lz0kKLLR0jy'),(1004,'emily_adams','emily.adams@example.com','$2y$10$84M6Rd7qLgEYyVGj3CYmm.xN2MNPYUdKnqMUTyOHgU3zZgt5OzA.6'),(1005,'william_clark','william.clark@example.com','$2y$10$7kP3eZogQJ4A66qCwhxih.vqETZHKEDbs8y2rUH2r4fHtxaIEE9y6');
 /*!40000 ALTER TABLE `employees` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -77,9 +77,9 @@ CREATE TABLE `meeting_details` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(10) NOT NULL,
   `duration` varchar(30) NOT NULL,
-  `date` date NOT NULL,
+  `meeting_date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,6 +88,7 @@ CREATE TABLE `meeting_details` (
 
 LOCK TABLES `meeting_details` WRITE;
 /*!40000 ALTER TABLE `meeting_details` DISABLE KEYS */;
+INSERT INTO `meeting_details` VALUES (1,'Meeting 1','01:00:00','2023-07-02 08:00:00'),(2,'Meeting 2','00:30:00','2023-07-03 08:00:00'),(3,'Meeting 3','02:00:00','2023-07-04 08:00:00');
 /*!40000 ALTER TABLE `meeting_details` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -102,7 +103,7 @@ CREATE TABLE `meeting_types` (
   `id` int NOT NULL AUTO_INCREMENT,
   `type` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -111,6 +112,7 @@ CREATE TABLE `meeting_types` (
 
 LOCK TABLES `meeting_types` WRITE;
 /*!40000 ALTER TABLE `meeting_types` DISABLE KEYS */;
+INSERT INTO `meeting_types` VALUES (1,'Internal'),(2,'External'),(3,'Hybrid'),(4,'Other');
 /*!40000 ALTER TABLE `meeting_types` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -134,7 +136,7 @@ CREATE TABLE `meetings` (
   CONSTRAINT `meetings_ibfk_1` FOREIGN KEY (`meeting_type_id`) REFERENCES `meeting_types` (`id`),
   CONSTRAINT `meetings_ibfk_2` FOREIGN KEY (`employee_no`) REFERENCES `employees` (`employee_no`),
   CONSTRAINT `meetings_ibfk_3` FOREIGN KEY (`meeting_details_id`) REFERENCES `meeting_details` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -143,6 +145,7 @@ CREATE TABLE `meetings` (
 
 LOCK TABLES `meetings` WRITE;
 /*!40000 ALTER TABLE `meetings` DISABLE KEYS */;
+INSERT INTO `meetings` VALUES (10,1,1001,1,'2023-07-02 06:00:00'),(11,2,1002,2,'2023-07-03 11:30:00'),(12,3,1003,3,'2023-07-04 08:00:00');
 /*!40000 ALTER TABLE `meetings` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -155,4 +158,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-02 15:16:19
+-- Dump completed on 2023-07-14 15:01:46
