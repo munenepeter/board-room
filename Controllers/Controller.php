f3049a9da45b205d64ee0e97882eb2ad907b1587
munenepeter <peternjega7@gmail.com>
2023-07-16 13:25:10 +0300

    reformat

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -14,24 +14,19 @@ use BoardRoom\Core\Mantle\Middleware;
 use BoardRoom\Core\Mantle\Request;
 use BoardRoom\Core\Mantle\Paginator;
 
-class Controller
-{
+class Controller {
 
 
-    public function middleware($middleware)
-    {
+    public function middleware($middleware) {
         return (new Middleware)->middleware($middleware);
     }
-    public function request()
-    {
+    public function request() {
         return (new Request);
     }
-    public function upload(array $file, string $location, int $max_size, array $mime_types)
-    {
+    public function upload(array $file, string $location, int $max_size, array $mime_types) {
         return $this->request()->upload($file, $location, $max_size, $mime_types);
     }
-    public function paginate(array $data, $per_page)
-    {
+    public function paginate(array $data, $per_page) {
         return Paginator::paginate($data, $per_page);
     }
     /**
@@ -42,11 +37,9 @@ class Controller
      * 
      * @return void
      */
-    public function json(mixed $values, int $code = 200): void
-    {
+    public function json(mixed $values, int $code = 200): void {
         header('Content-Type: application/json; charset=utf-8');
         http_response_code($code);
         echo json_encode($values, JSON_UNESCAPED_UNICODE);
     }
-
 }
