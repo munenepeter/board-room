2876203f6b9d8cd4ad522265c06f50880a5664d5
munenepeter <peternjega7@gmail.com>
2023-07-21 23:22:07 +0300

    update time

--- Views/sections/second-nav.view.php
+++ Views/sections/second-nav.view.php
@@ -72,7 +72,7 @@
                                 <input type="hidden" name="employee_no" value="<?= session_get('employee_no') ?>">
                                 <div>
                                     <label for="duration" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Duration</label>
-                                    <input type="text" name="duration" id="duration" required pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{2}" title="Write a duration in the format hh:mm:ss:ms" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="hh:mm:ss:ms">
+                                    <input type="text" name="duration" id="duration" required pattern="[0-9]{2}:[0-9]{2}" title="Write a duration in the format hh:mm" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="hh:mm:ss:ms">
                                 </div>
                             </div>
                             <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
